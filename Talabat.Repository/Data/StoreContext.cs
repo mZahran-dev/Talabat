35deb743f593091b28aca797f0cfd715c9322463
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-30 19:06:24 +0300

    feat : Add Store Context

---
+++ Talabat.Repository/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Repository.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+    }
+}
