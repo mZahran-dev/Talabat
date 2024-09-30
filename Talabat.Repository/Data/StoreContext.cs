bc1b33599d0b8563448a3d1f49687f9115ece9d7
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-30 19:19:58 +0300

    feat : Add DbSet for Product Models Apply Configuration

--- Talabat.Repository/Data/StoreContext.cs
+++ Talabat.Repository/Data/StoreContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Talabat.Core.Entities;
 
 namespace Talabat.Repository.Data
 {
@@ -12,6 +13,14 @@ namespace Talabat.Repository.Data
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+            //base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
 
     }
 }
