27ec779b6479dd455a073fb581de2d6262214c22
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-30 19:12:34 +0300

    feat : Configure Product Class

--- Talabat.Core/Entities/Product.cs
+++ Talabat.Core/Entities/Product.cs
@@ -8,7 +8,7 @@ namespace Talabat.Core.Entities
 {
     public class Product : BaseEntity
     {
-        string Name { get; set; }   
+        public string Name { get; set; }   
         public string Description { get; set; }
         public string PictureUrl { get; set; }
         public decimal Price { get; set; }
--- Talabat.Repository/Data/Configuration/ProductConfig.cs
+++ Talabat.Repository/Data/Configuration/ProductConfig.cs
@@ -13,7 +13,26 @@ namespace Talabat.Repository.Data.Configuration
     { 
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-            throw new NotImplementedException();
+            builder.Property(P => P.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+            builder.Property(P => P.PictureUrl)
+                   .IsRequired();
+
+            builder.Property(P => P.Price)
+                    .HasColumnType("decimal(18,2)");
+
+            builder.HasOne(P=>P.ProductBrand)
+                   .WithMany()
+                   .HasForeignKey(P => P.BrandId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(P => P.ProductCategory)
+                   .WithMany()
+                   .HasForeignKey(P => P.CategoryId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+
         }
     }
 }
