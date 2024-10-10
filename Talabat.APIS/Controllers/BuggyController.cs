9108d9578c279bf01666f00671064cd63edc4c08
Mahmoud Zahran <mi4654110@gmail.com>
2024-10-10 18:50:06 +0300

    feat : Create BuggyController

---
+++ Talabat.APIS/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Repository.Data;
+
+namespace Talabat.APIS.Controllers
+{
+    public class BuggyController : APIBaseController
+    {
+        private readonly StoreContext _dbContext;
+
+        public BuggyController(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var product = _dbContext.Products.Find(200);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerErrorRequest()
+        {
+            var product = _dbContext.Products.Find(100);
+            var productDto = product.ToString();
+            return Ok(productDto);
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+          return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
+
+
+
+    }
+}
---
+++ Talabat.APIS/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace Talabat.APIS.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public ApiResponse(int statusCode, string? message) 
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        private string? GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "bad Request",
+                401 => "UnAuthorized",
+                404 => "Not Found",
+                500 => "Server Error",
+                _ => null,
+            };
+        }  
+    }
+}
