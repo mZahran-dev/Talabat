c6436c520089212ea560f4fdbb4ede3944361ba9
Mahmoud Zahran <mi4654110@gmail.com>
2024-10-17 12:40:19 +0300

    feat : Create IBasketRepository

---
+++ Talabat.Core/Repositories.Contract/IBasketRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Repositories.Contract
+{
+    public interface IBasketRepository
+    {
+        public Task<CustomerBasket?> GetBasketAsync(string Basketid);
+        public Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket);
+        public Task<bool> DeleteBasketAsync(string BasketId);
+
+    }
+}
