4fe0c480d641565b42e8ac82eba6967881f6a92b
Mahmoud Zahran <mi4654110@gmail.com>
2024-10-18 12:47:04 +0300

    feat : Add AccountController

---
+++ Talabat.APIS/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIS.DTOs;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.APIS.Controllers
+{
+    public class AccountController : APIBaseController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        //public async Task<ActionResult<UserDto>> Login(LoginDto)
+        //{
+
+        //}
+
+
+
+    }
+}
