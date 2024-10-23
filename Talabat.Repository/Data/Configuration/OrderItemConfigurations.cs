cde9495e9309e48c5e6411c735ab5291e925b1a3
Mahmoud Zahran <mi4654110@gmail.com>
2024-10-23 14:05:29 +0300

    feat : Order Module Entities Configurations

---
+++ Talabat.Repository/Data/Configuration/OrderItemConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Order_Aggregate;
+
+namespace Talabat.Repository.Data.Configuration
+{
+    public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.OwnsOne(orderItem => orderItem.Product, product => product.WithOwner());
+            builder.Property(item => item.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
--- Talabat.Repository/Data/StoreContext.cs
+++ Talabat.Repository/Data/StoreContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
@@ -15,7 +16,7 @@ namespace Talabat.Repository.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             //base.OnModelCreating(modelBuilder);
         }
         public DbSet<Product> Products { get; set; }
