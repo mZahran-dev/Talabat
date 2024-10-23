70a532f7907aac03fadf1ecfbf2e9dcca4bbebc6
Mahmoud Zahran <mi4654110@gmail.com>
2024-10-23 16:43:39 +0300

    feat : Create IOrderService for Orderservice in Talabat.Services

---
+++ Talabat.Service/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Order_Aggregate;
+using Talabat.Core.Services.Contract;
+
+namespace Talabat.Service
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, string baskitId, DeliveryMethod deliveryMethod, Address shippingAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdForUserAsync(int orderId, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUsersAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
